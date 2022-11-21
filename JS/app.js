7f2faeec79d9b864a908b4efc176ea955f661b29
heneken1989 <116566386+heneken1989@users.noreply.github.com>
2022-11-22 00:46:21 +0900

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,33 @@
-var app = angular.module("myApp", [ "ngRoute",
-  "ngSanitize",
-  "dndLists",
-  "ngMessages",]);
+var app = angular.module("myApp", []);
+app.controller("ctrlAerobic", function ($scope) {
+  //   ------------
+  // -----------------------------------------------------------------
+});
+
+app.controller("HeaderFooter", function ($scope) {
+  $(document).ready(function () {
+    $(window).scroll(function () {
+      if ($(window).scrollTop() >= 30) {
+        $(".nav-bar").addClass("fixed-header");
+        $("header .logo").addClass("headerLogoTransition");
+        $(".logo").css("display", "none");
+        $(".logoAdd").css("display", "block");
+        $(".menu-icon").css("top", "-85px");
+      } else {
+        $(".nav-bar").removeClass("fixed-header");
+        $(".logoAdd").css("display", "none");
+        $("header .logo").removeClass("headerLogoTransition");
+        $(".logo").css("display", "block");
+        $(".menu-icon").css("top", "0px");
+      }
+    });
+  });
+  // =====================================
+});
+app.run(function ($rootScope, $http) {
+  $http.get("../JSON/product.json").then(function (rsp) {
+    $rootScope.products = rsp.data.product;
+    $rootScope.listPractice = [];
+    $rootScope.saveValue = [];
+  });
+});
