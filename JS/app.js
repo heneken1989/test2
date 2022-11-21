8cc454a96315be228d16d42d4184b814e1bcc69f
heneken1989 <116566386+heneken1989@users.noreply.github.com>
2022-11-21 23:41:56 +0900

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -11,7 +11,18 @@ app.config([
       .when("", { templateUrl: "./Page/home.html" })
       .when("/problem", { templateUrl: "../Page/problem.html" })
       .when("/meditation", { templateUrl: "../Page/meditation.html" })
-  };
+      .when("/", {
+        templateUrl: "../..//Page/customAerobic.html",
+        controller: "ctrlAerobic",
+      })
+      .when("/login", {
+        templateUrl: "../Page/login.html",
+        controller: "ctrlLogin",
+      })
+      .when("/displayDataLocal", {
+        templateUrl: "../Page/displayDataLocal.html",
+        controller: "ctrlLogin",
+      })
+
+  },
 ]);
-
-
