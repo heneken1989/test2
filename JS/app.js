f64fe57fd089d015fa5a69077f25e711c81be3dd
heneken1989 <116566386+heneken1989@users.noreply.github.com>
2022-11-22 00:03:21 +0900

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,16 @@
 var app = angular.module("myApp", [
+  "ngRoute",
+  "ngSanitize",
+  "dndLists",
+  "ngMessages",
+]);
+app.config([
+  "$routeProvider",
+  function ($routeProvider) {
+    $routeProvider
+      .when("", { templateUrl: "./Page/home.html" })
+      .when("/problem", { templateUrl: "../Page/problem.html" })
+      .when("/meditation", { templateUrl: "../Page/meditation.html" })
+
+  },
 ]);
